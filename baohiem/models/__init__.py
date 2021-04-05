35b4658a4a40921a45f8f74a7293fa6403871295
Leon Nguyen <58243410+khanhqn@users.noreply.github.com>
2021-04-05 08:05:54 +0700

    Revert "2nd commit"

--- baohiem/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import bao_hiem
-from . import dieu_chinh
-from . import lich_su
-from . import dchang_loat
-from . import cau_hinh
-from . import ketnoi_lichsu
-from . import cauhinh_vung
